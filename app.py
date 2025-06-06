673d138b132c7f6d2b0bcb9bd4cb6627076e66cb
R.Vomkesh Pavan <80672329+vomkeshpavan@users.noreply.github.com>
2025-06-07 02:59:31 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def submit():
         return redirect('/')
     return render_template('submit.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
